08b1133cb0e6c765df6226de74c01da2169838f5
Gaetano Santonastaso <gigapr@hotmail.com>
2019-01-31 23:36:01 +0000

    fixing

--- push-image-to-docker-hub.sh
+++ push-image-to-docker-hub.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+export ORGANISATION_NAME=threeamigos
+
 export BUILD_NUMBER=$1
 export REPOSITORY_NAME=$2
 export DOCKER_HUB_USER=$3
 export DOCKER_HUB_PASSWORD=$4
 
-export IMAGE_NAME=$REPOSITORY_NAME:$BUILD_NUMBER
+export IMAGE_NAME=$REPOSITORY_NAME
+export TAG=$ORGANISATION_NAME/$IMAGE_NAME:$BUILD_NUMBER
 
 docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $DOCKER_HUB_USER/$IMAGE_NAME
+docker tag $IMAGE_NAME $TAG
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-docker push threeamigos/$REPOSITORY_NAME:$IMAGE_NAME
+docker push $TAG
