e6257e341acc3795713e62616c893c1615cc692c
Gaetano Santonastaso <gigapr@hotmail.com>
2019-02-01 22:19:31 +0000

    try

--- push-image-to-docker-hub.sh
+++ push-image-to-docker-hub.sh
@@ -8,18 +8,18 @@ export BUILD_NUMBER=$1
 export IMAGE_NAME=$2
 export DOCKER_HUB_USER=$3
 export DOCKER_HUB_PASSWORD=$4
-export PUBLISH_TO_DOCKER_HUB=$5
+export BRANCH=$5
 
 export TAG=$ORGANISATION_NAME/$IMAGE_NAME:$BUILD_NUMBER
 
 docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $TAG
-docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 
-echo $PUBLISH_TO_DOCKER_HUB
+echo $BRANCH
 
-if [ $PUBLISH_TO_DOCKER_HUB ]
+if [ $BRANCH = 'master']
 then
+    docker tag $IMAGE_NAME $TAG
+    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
     docker push $TAG
 fi
 
