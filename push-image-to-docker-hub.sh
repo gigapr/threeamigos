79d4d1ff017fcdccd42879b50fe1ad7595d322ac
Gaetano Santonastaso <gigapr@hotmail.com>
2019-02-01 22:03:34 +0000

    Updated TrafficSignal readme

--- push-image-to-docker-hub.sh
+++ push-image-to-docker-hub.sh
@@ -8,11 +8,19 @@ export BUILD_NUMBER=$1
 export IMAGE_NAME=$2
 export DOCKER_HUB_USER=$3
 export DOCKER_HUB_PASSWORD=$4
+export PUBLISH_TO_DOCKER_HUB=$5
 
 export TAG=$ORGANISATION_NAME/$IMAGE_NAME:$BUILD_NUMBER
 
 docker build -t $IMAGE_NAME .
 docker tag $IMAGE_NAME $TAG
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-docker push $TAG
+
+echo '$PUBLISH_TO_DOCKER_HUB'
+
+if [ $PUBLISH_TO_DOCKER_HUB ]
+then
+    docker push $TAG
+fi
+
 docker logout
