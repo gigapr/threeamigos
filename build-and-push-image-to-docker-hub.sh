6ab893dd4d16bdaa098ec686c6a6e5e3015ee91e
Gaetano Santonastaso <gigapr@hotmail.com>
2019-02-01 22:30:02 +0000

    It only pushes TrafficSignal image to dockerhub if built from master (#6)

--- build-and-push-image-to-docker-hub.sh
+++ build-and-push-image-to-docker-hub.sh
@@ -8,11 +8,19 @@ export BUILD_NUMBER=$1
 export IMAGE_NAME=$2
 export DOCKER_HUB_USER=$3
 export DOCKER_HUB_PASSWORD=$4
+export BRANCH=$5
 
 export TAG=$ORGANISATION_NAME/$IMAGE_NAME:$BUILD_NUMBER
 
 docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $TAG
-docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-docker push $TAG
+
+echo $BRANCH
+
+if [ $BRANCH = 'master']
+then
+    docker tag $IMAGE_NAME $TAG
+    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+    docker push $TAG
+fi
+
 docker logout
