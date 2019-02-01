c77065b755d3c2762172dbb85b2f11f485d256c1
Gaetano Santonastaso <gigapr@hotmail.com>
2019-02-01 00:10:56 +0000

    In TravisCI it creates a docker image and pushes to docker hub (#5)

---
+++ push-image-to-docker-hub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o xtrace               #prints commands to console
+
+export ORGANISATION_NAME=threeamigos
+
+export BUILD_NUMBER=$1
+export IMAGE_NAME=$2
+export DOCKER_HUB_USER=$3
+export DOCKER_HUB_PASSWORD=$4
+
+export TAG=$ORGANISATION_NAME/$IMAGE_NAME:$BUILD_NUMBER
+
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $TAG
+docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+docker push $TAG
+docker logout
