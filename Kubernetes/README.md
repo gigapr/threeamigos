b77b2617ee34f1c16ee765ff0cfed90dcea398c6
Gaetano Santonastaso <gigapr@hotmail.com>
2019-03-05 22:17:08 +0000

    Fixed kubernetes scripts

--- Kubernetes/README.md
+++ Kubernetes/README.md
@@ -2,10 +2,4 @@
 
 - Install Istio in Kubernetes
 
-- `kubectl create -f ./threeamigos-namespace.yaml`
-
-- `kubectl label namespace threeamigos istio-injection=enabled`
-
-- `kubectl --namespace=threeamigos apply -f ./MongoDb/minikube_hostpath.yaml`
-
-- `kubectl --namespace=threeamigos apply -f ./MongoDb/mongo-statefulset.yaml`
+- `kubectl apply -f ./src`
--- TrafficSignalsConfigurator/src/TrafficSignalsConfigurator.Web/ViewModels/AuthDataViewModel 2.cs
+++ TrafficSignalsConfigurator/src/TrafficSignalsConfigurator.Web/ViewModels/AuthDataViewModel 2.cs
@@ -14,15 +14,23 @@ namespace TrafficSignalsConfigurator.Web.ViewModels
 
         public AuthDataViewModel(string jwtSecret, int jwtLifespan, string userId)
         {
-            if(string.IsNullOrEmpty(jwtSecret)) throw new ArgumentNullException(nameof(jwtSecret));
-            if(jwtLifespan <= 0) throw new ArgumentException($"'{nameof(jwtLifespan)}' cannot be less than 1. Current value {jwtLifespan}");
-            if(string.IsNullOrEmpty(userId)) throw new ArgumentNullException(nameof(userId));
+            if (string.IsNullOrEmpty(jwtSecret)) throw new ArgumentNullException(nameof(jwtSecret));
+            if (jwtLifespan <= 0) throw new ArgumentException($"'{nameof(jwtLifespan)}' cannot be less than 1. Current value {jwtLifespan}");
+            if (string.IsNullOrEmpty(userId)) throw new ArgumentNullException(nameof(userId));
 
             var expirationTime = DateTime.UtcNow.AddSeconds(jwtLifespan);
+            var token = CreateJwtSecurityToken(jwtSecret, jwtLifespan, userId, expirationTime);
 
+            Token = token;
+            TokenExpirationTime = ((DateTimeOffset)expirationTime).ToUnixTimeSeconds();
+            UserId = userId;
+        }
+
+        private static string CreateJwtSecurityToken(string jwtSecret, int jwtLifespan, string userId, DateTime expirationTime)
+        {
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userId)}),
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userId) }),
                 Expires = expirationTime,
                 // new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret)), SecurityAlgorithms.HmacSha256Signature)
@@ -31,9 +39,7 @@ namespace TrafficSignalsConfigurator.Web.ViewModels
             var tokenHandler = new JwtSecurityTokenHandler();
             var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
 
-            Token = token;
-            TokenExpirationTime = ((DateTimeOffset) expirationTime).ToUnixTimeSeconds();
-            UserId = userId;
+            return token;
         }
     }
 }
